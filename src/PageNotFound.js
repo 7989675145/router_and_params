151090cfbb1c4d1269b95cbb9a7c00e3e903aac6
hemanth <hemanthgali7@gmail.com>
2022-04-30 00:51:42 +0530

    update pagenotfound

---
+++ src/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+      <center>
+          <h1>404 Error Page not found</h1>
+      </center>    
+  )
+}
+
+export default PageNotFound
