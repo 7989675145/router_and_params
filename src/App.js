c9d5777b32854027b2af776e5e7dcef6e0200af6
hemanth <hemanthgali7@gmail.com>
2022-04-30 00:54:50 +0530

    update app with page not found

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Home from './Home';
 import About from './About';
+import PageNotFound from './PageNotFound';
 const App = () => {
   return (
     <div>
@@ -11,6 +12,7 @@ const App = () => {
           <Route path='/' element={<Home />}/>
           <Route path='/dashboard/:name' element={<Dashboard />}/> {/* for path param */}
           <Route path='/about' element={<About />}/> {/* for query param */}
+          <Route path='*' element={<PageNotFound />}/>
         </Routes>
       </Router>
     </div>
